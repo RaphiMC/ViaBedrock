7fe2884313a9dea7ada750098bc389d4f9c88ac9
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-06-17 16:30:50 +0200

    Finished command block data translation

--- src/main/java/net/raphimc/viabedrock/protocol/rewriter/blockentity/CommandBlockBlockEntityRewriter.java
+++ src/main/java/net/raphimc/viabedrock/protocol/rewriter/blockentity/CommandBlockBlockEntityRewriter.java
@@ -20,12 +20,16 @@ package net.raphimc.viabedrock.protocol.rewriter.blockentity;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.minecraft.blockentity.BlockEntity;
 import com.viaversion.viaversion.api.minecraft.blockentity.BlockEntityImpl;
-import com.viaversion.viaversion.libs.opennbt.tag.builtin.ByteTag;
-import com.viaversion.viaversion.libs.opennbt.tag.builtin.CompoundTag;
-import com.viaversion.viaversion.libs.opennbt.tag.builtin.IntTag;
-import com.viaversion.viaversion.libs.opennbt.tag.builtin.StringTag;
+import com.viaversion.viaversion.libs.opennbt.tag.builtin.*;
+import net.lenni0451.mcstructs_bedrock.text.utils.BedrockTranslator;
 import net.raphimc.viabedrock.api.chunk.BedrockBlockEntity;
+import net.raphimc.viabedrock.api.util.TextUtil;
 import net.raphimc.viabedrock.protocol.rewriter.BlockEntityRewriter;
+import net.raphimc.viabedrock.protocol.storage.ResourcePacksStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 public class CommandBlockBlockEntityRewriter implements BlockEntityRewriter.Rewriter {
 
@@ -37,11 +41,29 @@ public class CommandBlockBlockEntityRewriter implements BlockEntityRewriter.Rewr
         copy(bedrockTag, javaTag, "Command", StringTag.class);
         copy(bedrockTag, javaTag, "SuccessCount", IntTag.class);
         copy(bedrockTag, javaTag, "TrackOutput", ByteTag.class);
+        copy(bedrockTag, javaTag, "LastExecution", LongTag.class);
         copy(bedrockTag, javaTag, "powered", ByteTag.class);
         copy(bedrockTag, javaTag, "conditionMet", ByteTag.class);
         copy(bedrockTag, javaTag, "auto", ByteTag.class);
-
         translateCustomName(user, bedrockTag, javaTag);
+
+        if (bedrockTag.get("LastOutput") instanceof StringTag) {
+            final Function<String, String> translator = k -> user.get(ResourcePacksStorage.class).getTranslations().getOrDefault(k, k);
+
+            final List<String> lastOutputParams = new ArrayList<>();
+            if (bedrockTag.get("LastOutputParams") instanceof ListTag) {
+                final ListTag bedrockLastOutputParams = bedrockTag.get("LastOutputParams");
+                if (bedrockLastOutputParams.getElementType().equals(StringTag.class)) {
+                    for (Tag bedrockLastOutputParam : bedrockLastOutputParams) {
+                        lastOutputParams.add(((StringTag) bedrockLastOutputParam).getValue());
+                    }
+                }
+            }
+
+            final String bedrockLastOutput = bedrockTag.<StringTag>get("LastOutput").getValue();
+            final String javaLastOutput = TextUtil.stringToJson(BedrockTranslator.translate(bedrockLastOutput, translator, lastOutputParams.toArray()));
+            javaTag.put("LastOutput", new StringTag(javaLastOutput));
+        }
 
         return new BlockEntityImpl(bedrockBlockEntity.packedXZ(), bedrockBlockEntity.y(), -1, javaTag);
     }
