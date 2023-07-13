278a8e7dd9d7610b0a4d1748e6521473b77c5a8b
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-07-13 17:54:51 +0200

    Fixed chunk errors in <= 1.16.5

--- src/main/java/net/raphimc/viabedrock/protocol/storage/ChunkTracker.java
+++ src/main/java/net/raphimc/viabedrock/protocol/storage/ChunkTracker.java
@@ -35,7 +35,7 @@ import com.viaversion.viaversion.libs.opennbt.tag.builtin.ListTag;
 import com.viaversion.viaversion.libs.opennbt.tag.builtin.NumberTag;
 import com.viaversion.viaversion.protocols.protocol1_18to1_17_1.types.Chunk1_18Type;
 import com.viaversion.viaversion.protocols.protocol1_19_4to1_19_3.ClientboundPackets1_19_4;
-import com.viaversion.viaversion.protocols.protocol1_19_4to1_19_3.Protocol1_19_4To1_19_3;
+import com.viaversion.viaversion.protocols.protocol1_20to1_19_4.Protocol1_20To1_19_4;
 import com.viaversion.viaversion.util.MathUtil;
 import net.raphimc.viabedrock.ViaBedrock;
 import net.raphimc.viabedrock.api.chunk.BedrockBlockEntity;
@@ -110,7 +110,7 @@ public class ChunkTracker extends StoredObject {
         this.minY = pair.keyInt();
         this.worldHeight = pair.valueInt();
 
-        this.chunkType = new Chunk1_18Type(this.worldHeight >> 4, MathUtil.ceilLog2(Protocol1_19_4To1_19_3.MAPPINGS.getBlockStateMappings().mappedSize()), MathUtil.ceilLog2(biomes.size()));
+        this.chunkType = new Chunk1_18Type(this.worldHeight >> 4, MathUtil.ceilLog2(Protocol1_20To1_19_4.MAPPINGS.getBlockStateMappings().mappedSize()), MathUtil.ceilLog2(biomes.size()));
     }
 
     public void setCenter(final int x, final int z) throws Exception {
