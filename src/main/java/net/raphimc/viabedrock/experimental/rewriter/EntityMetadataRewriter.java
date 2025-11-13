9e6f014e63df84d8ef21105e2a984939a755fbfc
Exterminate <Exterminate5573@gmail.com>
2025-11-13 22:25:02 +1100

    Cleanup
    Co-authored-by: RaphiMC <50594595+raphimc@users.noreply.github.com>

--- src/main/java/net/raphimc/viabedrock/experimental/rewriter/EntityMetadataRewriter.java
+++ src/main/java/net/raphimc/viabedrock/experimental/rewriter/EntityMetadataRewriter.java
@@ -22,8 +22,10 @@ import com.viaversion.viaversion.api.type.types.version.VersionedTypes;
 import net.raphimc.viabedrock.ViaBedrock;
 import net.raphimc.viabedrock.api.model.entity.Entity;
 import net.raphimc.viabedrock.protocol.data.enums.bedrock.generated.ActorDataIDs;
+import net.raphimc.viabedrock.protocol.data.enums.bedrock.generated.ActorFlags;
 
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 
 public class EntityMetadataRewriter {
@@ -32,20 +34,36 @@ public class EntityMetadataRewriter {
     public static boolean rewrite(final Entity entity, final ActorDataIDs id, final EntityData entityData, final List<EntityData> javaEntityData) {
 
         EntityData javaData = switch (id) {
-            case RESERVED_0 -> { // Some sort of movement bitmask seems to line up with https://minecraft.wiki/w/Java_Edition_protocol/Entity_metadata#Entity
+            case RESERVED_0, RESERVED_092 -> { // Entity flags mask
+                Set<ActorFlags> bedrockFlags = entity.entityFlags();
+                byte javaBitMask = 0; // https://minecraft.wiki/w/Java_Edition_protocol/Entity_metadata#Entity
+                if (bedrockFlags.contains(ActorFlags.ONFIRE)) {
+                    javaBitMask |= (1 << 0);
+                }
+                if (bedrockFlags.contains(ActorFlags.SNEAKING)) {
+                    javaBitMask |= (1 << 1);
+                }
+                if (bedrockFlags.contains(ActorFlags.RIDING)) {
+                    javaBitMask |= (1 << 2);
+                }
+                if (bedrockFlags.contains(ActorFlags.SPRINTING)) {
+                    javaBitMask |= (1 << 3);
+                }
+                if (bedrockFlags.contains(ActorFlags.SWIMMING)) {
+                    javaBitMask |= (1 << 4);
+                }
+                if (bedrockFlags.contains(ActorFlags.INVISIBLE)) {
+                    javaBitMask |= (1 << 5);
+                }
 
-                long bedrockBits = (long) entityData.getValue();
-                byte javaBitMask = (byte) bedrockBits;
-
-                //TODO: Handle the other bits properly?
-                long remaining = bedrockBits >>> 8;
+                //TODO: Handle the other flags properly?
                 //ViaBedrock.getPlatform().getLogger().log(Level.WARNING, "Unhandled movement bitmask bits for entity " + entity.type() + ": " + Long.toBinaryString(remaining));
 
-                yield new EntityData(0, VersionedTypes.V1_21_9.entityDataTypes().byteType, javaBitMask);
+                yield new EntityData(entity.getJavaEntityDataIndex("SHARED_FLAGS"), VersionedTypes.V1_21_9.entityDataTypes().byteType, javaBitMask);
             }
             case AIR_SUPPLY -> { // Air supply is stored as a short in Bedrock, but an int in Java (Bedrock also has a max air supply value we ignore for now)
                 short airSupply = (short) entityData.getValue();
-                yield new EntityData(1, VersionedTypes.V1_21_9.entityDataTypes().varIntType, (int) airSupply);
+                yield new EntityData(entity.getJavaEntityDataIndex("AIR_SUPPLY"), VersionedTypes.V1_21_9.entityDataTypes().varIntType, (int) airSupply);
             }
             case POSE_INDEX -> null; // TODO: Armour stand pose index
             default -> null;
